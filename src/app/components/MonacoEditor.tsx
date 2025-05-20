480c35b1384ddfcf3ef097fdc8265aaaabf9a509
TeppuSan <suraimu0206@icloud.com>
2025-05-20 18:00:36 +0900

    エディタ表示

--- src/app/components/MonacoEditor.tsx
+++ src/app/components/MonacoEditor.tsx
@@ -3,9 +3,10 @@
 import { Editor } from "@monaco-editor/react";
 
 import "./Monaco.css";
-import { useEffect,useRef } from "react";
+import { useEffect,useRef,useState } from "react";
 useEffect
 useRef
+useState
 
 Editor
 
@@ -27,30 +28,65 @@ export default function MonacoEditor(){
     }
   }
     
+  function txtdownload() {
+    if (editorRef.current) {
+      const code = editorRef.current.getValue(); // エディタの内容を取得
+      const blob = new Blob([code],{type:"text/plain"});
+      const dl = URL.createObjectURL(blob);
+      const a =document.createElement("a");
+      a.href=dl;
+      a.download="sample.html";//ここでダウンロードの指定
+      a.click();
+      URL.revokeObjectURL(dl);
+    }
+  }
+    
     return(
         <div className="box">
-        <div className="Editor" id="Edit">
-            <Editor
-            height="500px"
-            defaultLanguage="html"
-            defaultValue='console.log("Helloweed")'
-            theme="vs-dark"
-            onMount={handleEditorDidMount}
+            <div className="Editor" id="Edit">
+                <Editor
+                height="500px"
+                defaultLanguage="html"
+                defaultValue=
+{`<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <main>これはサンプルです</main>
+    </body>
+</html>`}
+                value={code}
+                onChange={}
+                theme="vs-dark"
+                onMount={handleEditorDidMount}
 
-            />
-        </div>
-        <button onClick=
-        {handleClick}
-        >これはボタンです</button>
-        
-        <div className="result">
-            <Editor
-            height="500px"
-            defaultLanguage="javascript"
-            defaultValue='console.log("Helloword")'
-            theme="vs-dark"
-            />
-        </div>
+                />
+            </div>
+            <div className="buttonbox">
+                <button onClick=
+                {handleClick}
+                >これはalertボタンです</button>
+                <button onClick=
+                {txtdownload}
+                >これはtxt保存ボタンです</button>
+            </div>  
+            <div className="result">
+                <iframe
+                srcDoc={code}
+                title="now"
+                sandbox=""
+                />
+                {/* <Editor
+                height="500px"
+                defaultLanguage="javascript"
+                defaultValue='console.log("Helloword")'
+                theme="vs-dark"
+                /> */}
+            </div>
         </div>
     )
 }
